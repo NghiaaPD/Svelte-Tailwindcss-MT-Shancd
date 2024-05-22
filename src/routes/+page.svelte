ad09119a77ff58d2b107b870fefc9aeef420fdcf
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2024-05-22 09:51:20 +0700

    add code config

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Button } from "../lib/components/ui/button";
+    import Sun from "svelte-radix/Sun.svelte";
+    import Moon from "svelte-radix/Moon.svelte";
+    import { toggleMode } from "mode-watcher";
+</script>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+    <Sun
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Moon
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+</Button>
+
+<h1 class="text-3xl font-bold underline bg-red-500 ">
+    Hello world!
+</h1>
+
+<button
+        type="button"
+        data-ripple-light="true"
+        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+>
+    Button
+</button>
+
+<style lang="postcss">
+
+</style>
